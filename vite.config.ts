f5f2227386e1a004ff6f469610b12cbdcfc88d6e
MuhammadHarits <wert640559@gmail.com>
2026-01-06 21:07:20 +0700

    fetch api

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/api-v1': {
-        target: 'https://halaqahid-backend.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-v1/, '')
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api-v1': {
+  //       target: 'https://halaqahid-backend.vercel.app',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api-v1/, '')
+  //     }
+  //   }
+  // }
 })
